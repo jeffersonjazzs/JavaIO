14ab17ca8c306e448df7e84d6cbb0410ed4b278a
jeffersonjazzs <jefferson_jazzs@hotmail.com>
2020-08-02 17:21:40 -0300

    update

--- src/com/br/itimagine/FileDemo.java
+++ src/com/br/itimagine/FileDemo.java
@@ -1,12 +1,33 @@
 package com.br.itimagine;
 
 import java.io.File;
+import java.text.DateFormat;
 
 public class FileDemo {
+	static void p(String s) {
+		System.out.println(s);
+	}
+	
 	
 	public static void main(String[] args) {
-		File f1 = new File("/java");
+		File f1 = new File("/java/arquivo.txt");
+		p("nome do arquivo: "+ f1.getName());
+		p("caminho do arquivo: "+ f1.getPath());
+		p("caminho absoluto: "+ f1.getAbsolutePath());
+		p("Nivel acima: "+ f1.getParent());
+		p(f1.exists() ? "existe" : "não existe");
+		p(f1.canWrite() ? "permite escritura" :  "nao permite");
+		p(f1.canRead() ? "leitura" : "nao permite leitura");
+		p(f1.isDirectory() ? "È" : "nao é " + "um diretorio");
+		p(f1.isFile() ? "é um arquivo" : "não pode ser");
+		p("Tamanho " + f1.length());
+		p("ultima modificação " + f1.lastModified());
+	
+		long d =f1.lastModified();
+	     
+       DateFormat df = DateFormat.getDateTimeInstance();
+       System.out.println("ultima modificação: " + df.format(d));
+
 
 	}
-
 }
